5a9d3774c1d57c1b317b3f1fa113f54ba64cb569
신관규 <soundbar91@gmail.com>
2024-01-01 15:45:37 +0900

    [BOJ] 조합 0의 개수

--- BOJ/src/Silver/II/BOJ_2004.java
+++ BOJ/src/Silver/II/BOJ_2004.java
@@ -2,7 +2,6 @@ package Silver.II;
 
 import java.io.*;
 import java.util.StringTokenizer;
-
 public class BOJ_2004 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
