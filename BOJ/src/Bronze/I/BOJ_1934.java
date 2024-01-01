232be82f15871686d7230de4a7e818336955e8c7
신관규 <soundbar91@gmail.com>
2024-01-01 15:37:59 +0900

    [BOJ] 최소공배수

--- BOJ/src/Bronze/I/BOJ_1934.java
+++ BOJ/src/Bronze/I/BOJ_1934.java
@@ -2,7 +2,6 @@ package Bronze.I;
 
 import java.io.*;
 import java.util.StringTokenizer;
-
 public class BOJ_1934 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
