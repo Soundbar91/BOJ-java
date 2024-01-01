e00cd47f78f5766522ad7f0295c1a72456c2cbba
신관규 <soundbar91@gmail.com>
2024-01-01 15:40:28 +0900

    [BOJ] 네 수

--- BOJ/src/Bronze/III/BOJ_10824.java
+++ BOJ/src/Bronze/III/BOJ_10824.java
@@ -2,7 +2,6 @@ package Bronze.III;
 
 import java.io.*;
 import java.util.StringTokenizer;
-
 public class BOJ_10824 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
