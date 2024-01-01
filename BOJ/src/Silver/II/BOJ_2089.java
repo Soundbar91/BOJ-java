17c40d80f8c21b146bb888f8a398e24c25d5a7d0
신관규 <soundbar91@gmail.com>
2024-01-01 15:46:01 +0900

    [BOJ] -2진수

--- BOJ/src/Silver/II/BOJ_2089.java
+++ BOJ/src/Silver/II/BOJ_2089.java
@@ -1,7 +1,6 @@
 package Silver.II;
 
 import java.io.*;
-
 public class BOJ_2089 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
