a992b82308821f2cdd7b77b5b95148d3db03250b
신관규 <soundbar91@gmail.com>
2024-01-01 15:52:41 +0900

    [BOJ] 큐

--- BOJ/src/Silver/IV/BOJ_10845.java
+++ BOJ/src/Silver/IV/BOJ_10845.java
@@ -7,7 +7,6 @@ public class BOJ_10845 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         String str;
         int n = Integer.parseInt(br.readLine());
 
