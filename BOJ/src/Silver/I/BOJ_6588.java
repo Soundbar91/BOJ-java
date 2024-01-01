5eb4773e5a5dc40191edbe9c81c998fe1c17cc8d
신관규 <soundbar91@gmail.com>
2024-01-01 15:42:39 +0900

    [BOJ] 골드바흐의 추측

--- BOJ/src/Silver/I/BOJ_6588.java
+++ BOJ/src/Silver/I/BOJ_6588.java
@@ -1,7 +1,6 @@
 package Silver.I;
 
 import java.io.*;
-
 public class BOJ_6588 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
