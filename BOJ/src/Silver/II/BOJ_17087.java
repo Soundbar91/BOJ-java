ba339b54df2aa0e7883a4a76b42f6e8ce932cc41
신관규 <soundbar91@gmail.com>
2024-01-01 15:44:20 +0900

    [BOJ] 숨바꼭질 6

--- BOJ/src/Silver/II/BOJ_17087.java
+++ BOJ/src/Silver/II/BOJ_17087.java
@@ -2,7 +2,6 @@ package Silver.II;
 
 import java.io.*;
 import java.util.StringTokenizer;
-
 public class BOJ_17087 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
