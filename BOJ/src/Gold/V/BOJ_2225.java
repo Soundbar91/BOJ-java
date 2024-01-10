7c311ede80f13429ab8cc7d25409be5b68960d6a
신관규 <soundbar91@gmail.com>
2024-01-10 09:14:06 +0900

    [BOJ] 합분해

--- BOJ/src/Gold/V/BOJ_2225.java
+++ BOJ/src/Gold/V/BOJ_2225.java
@@ -6,8 +6,8 @@ import java.util.StringTokenizer;
 public class BOJ_2225 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int[][] dp = new int[201][201];
